95cd37a5f0a75ef19095255914ad033c4006c1eb
B4rrrry <slik_003@mail.ru>
2021-11-23 23:02:13 +0600

    ;fld;f

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,11 @@ function modalRequest() {
   const buyBtn = document.querySelectorAll(".buy__btn"),
     modalWrap = document.querySelector(".modal__request-wrap"),
     modalForm = document.querySelector(".modal__request-form"),
-    modalClose = document.querySelector(".modal__request-close");
+    modalClose = document.querySelector(".modal__request-close"),
+    headerMobile = document.querySelector('.header__mobile-req');
+  headerMobile.addEventListener('click', function () {
+    openModal();
+  })
   modalClose.addEventListener("click", function () {
     closeModal();
   });
@@ -11,16 +15,21 @@ function modalRequest() {
     modalWrap.classList.remove("modal__request-wrap-active");
     modalForm.classList.remove("modal__request-form-active");
   }
+  function openModal () {
+    modalWrap.classList.add("modal__request-wrap-active");
+    modalForm.classList.add("modal__request-form-active");
+    modalWrap.addEventListener('click', function (e) {
+      if(e.target != modalForm) {
+          closeModal(); 
+          document.removeEventListener('click', this)
+      }
+  })
+  }
   buyBtn.forEach((btn, i) => {
     btn.addEventListener("click", function () {
-      modalWrap.classList.add("modal__request-wrap-active");
-      modalForm.classList.add("modal__request-form-active");
+      openModal();
+      
     });
   });
-  modalWrap.addEventListener('click', function (e) {
-    if(e.target != modalForm) {
-        closeModal(); 
-        document.removeEventListener('click', this)
-    }
-})
+
 }
